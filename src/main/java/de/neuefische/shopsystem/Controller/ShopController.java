26370d7435c696fb8435a7e04d8100f4f3c76584
Dirk Stadge <dirk_stadge@yahoo.de>
2023-05-26 16:28:27 +0200

    Add GetMapping in ShopController

--- src/main/java/de/neuefische/shopsystem/Controller/ShopController.java
+++ src/main/java/de/neuefische/shopsystem/Controller/ShopController.java
@@ -1,9 +1,15 @@
 
 package de.neuefische.shopsystem.Controller;
 
+import de.neuefische.shopsystem.model.Order;
+import de.neuefische.shopsystem.model.Product;
 import de.neuefische.shopsystem.service.ShopService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -15,7 +21,23 @@ public class ShopController {
     public ShopController(ShopService shopservice) {
         this.shopservice = shopservice;
     }
+@GetMapping ("/api/shop/products")
+    public List<Product> getAllProducts(){
+        return shopservice.listProducts();
+}
+@GetMapping("/api/shop/products/{id}")
+    public Product getPoductById(@PathVariable String id){
+    return shopservice.getProductById(id);
+}
 
+@GetMapping("api/shop/orders")
+    public List<Order> getAllOrders(){
+        return shopservice.listOrders();
+}
 
+    @GetMapping("/api/shop/orders/{id}")
+    public Order getOrderById(@PathVariable String id){
+        return shopservice.getOrderById(id);
+    }
 
 }
